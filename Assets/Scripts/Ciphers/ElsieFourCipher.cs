c34728cef69bc2c96218fbca89d5c53a328aea3d
Sean Obach <obachs971@strose.edu>
2024-03-05 09:23:45 -0500

    ElsieFour Cipher fix

--- Assets/Scripts/Ciphers/ElsieFourCipher.cs
+++ Assets/Scripts/Ciphers/ElsieFourCipher.cs
@@ -34,12 +34,14 @@ public class ElsieFourCipher : CipherBase
         logMessages.Add(string.Format("Screen 2: {0}", replaceZ));
 
         string kw = new Data().PickWord(4, 8);
+        //kw = "ADVOCACY";
         var keyFront = CMTools.generateBoolExp(bomb);
         string key = CMTools.getKey(kw.Replace("Z", ""), alpha, keyFront.Value);
         logMessages.Add(string.Format("Keyword: {0}", kw));
         logMessages.Add(string.Format("Keyword Front Rule: {0} -> {1}", keyFront.Expression, keyFront.Value));
         logMessages.Add(string.Format("Key: {0}", key));
         string markedLetters = key[Random.Range(0, alpha.Length)] + "";
+        //markedLetters = "T";
         logMessages.Add(string.Format("Screen B: {0}", markedLetters));
         if (invert)
         {
@@ -72,7 +74,7 @@ public class ElsieFourCipher : CipherBase
                 col = (col + (val % 5)) % 5;
                 row = (row + (val / 5)) % 5;
                 markedLetters += key[(row * 5) + col];
-                logMessages.Add(string.Format("New Marked Letter: {0}", markedLetters[markedLetters.Length - 1]));
+                logMessages.Add(string.Format("New Marked Letter: {0} + {1} -> {2}", markedLetters[markedLetters.Length - 2], encrypt[encrypt.Length - 1], markedLetters[markedLetters.Length - 1]));
             }
         }
         else
@@ -83,8 +85,8 @@ public class ElsieFourCipher : CipherBase
                 int index = key.IndexOf(letter);
                 int row = index / 5, col = index % 5;
                 int val = markedLetters[markedLetters.Length - 1] - 'A';
-                col = (col + (val % 5)) % 5;
-                row = (row + (val / 5)) % 5;
+                col = CMTools.mod(col + (val % 5), 5);
+                row = CMTools.mod(row + (val / 5), 5);
                 encrypt += key[(row * 5) + col];
                 logMessages.Add(string.Format("{0} + {1} -> {2}", letter, markedLetters[markedLetters.Length - 1], encrypt[encrypt.Length - 1]));
                 //Change Key
@@ -106,7 +108,7 @@ public class ElsieFourCipher : CipherBase
                 col = (col + (val % 5)) % 5;
                 row = (row + (val / 5)) % 5;
                 markedLetters += key[(row * 5) + col];
-                logMessages.Add(string.Format("New Marked Letter: {0}", markedLetters[markedLetters.Length - 1]));
+                logMessages.Add(string.Format("New Marked Letter: {0} + {1} -> {2}", markedLetters[markedLetters.Length - 2], encrypt[encrypt.Length - 1], markedLetters[markedLetters.Length - 1]));
             }
         }
        
